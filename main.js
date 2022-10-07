ffe3fcf4827e963ff0b8389832151725a91541c8
Jacob Thomas <jacgretho@gmail.com>
2022-10-07 09:43:35 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const add = (num1, num2) => {
+        return num1 + num2
+}
+
+const multiplyBy2 = (num) => {
+    return num * 2
+}
+
+let sum = add(42, 77)
+console.log(sum)
+
+let product = multiplyBy2(119)
+console.log(product)
